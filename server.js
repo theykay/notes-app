58c276b5d52d028b85509650dd33c3cbc0753c14
TheyKay <murraykyleb@gmail.com>
2020-10-06 11:01:39 -0600

    started server js file and created routes folder with html routes file

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
