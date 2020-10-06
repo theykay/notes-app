f46d3745500550aa5ee0d2d91dedcdd35f5827c0
TheyKay <murraykyleb@gmail.com>
2020-10-06 14:59:55 -0600

    working on api delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,38 @@
 const data = require("../db/db.json");
 const fs = require("fs");
 const dbPath = "./db/db.json";
+const dbContent = fs.readFileSync(dbPath);
 
 module.exports = function(app) {
     app.get("/api/notes", (req, res) => {
         res.json(data);
     });
     app.post("/api/notes", (req, res) => {
-        const dbFile = JSON.parse(fs.readFileSync(dbPath));
-        dbFile.push(req.body);
-        fs.writeFileSync(dbPath, JSON.stringify(dbFile, null, 2));
+        const notes = JSON.parse(dbContent);
+        const newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: data.length.toString()
+        };
+        notes.push(newNote);
+        fs.writeFileSync(dbPath, JSON.stringify(notes, null, 2));
+        res.json(newNote);
     });
     app.delete("/api/notes/:id", (req, res) => {
-
+        // console.log('hello');
+        // res.json(req.body);
+        let notes = JSON.parse(dbContent);
+        notes = notes.filter((note) => {
+            return note.id != req.params.id;
+        })
+        reassign(notes);
+        fs.writeFileSync(dbPath, JSON.stringify(notes, null, 2));
+        res.json(notes);
     })
+    
+    function reassign(notes) {
+        notes = notes.forEach((note, index) => {
+            note["id"] = index.toString();
+        });
+    }
 }
