1f51db9f01e981767419f022ce6c5856e88f753a
TheyKay <murraykyleb@gmail.com>
2020-10-06 12:27:40 -0600

    fixing paths, apiroutes post working ish

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,17 @@
 const data = require("../db/db.json");
 const fs = require("fs");
+const dbPath = "./db/db.json";
 
 module.exports = function(app) {
     app.get("/api/notes", (req, res) => {
         res.json(data);
     });
     app.post("/api/notes", (req, res) => {
-        const id = data.length;
         console.log(req.body);
-        // fs.appendFileSync(data, req.body);
+        const dbFile = JSON.parse(fs.readFileSync(dbPath));
+        dbFile.push(req.body);
+        console.log(dbFile);
+        fs.writeFileSync(dbPath, JSON.stringify(dbFile, null, 2));
         // res.json(true);
     });
     app.delete("/api/notes/:id", (req, res) => {
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use( express.static( __dirname + '/public' ));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
